0028133a52c94bbd456ca4546d8c32578514632b
Godbless <mygodblesspersonal@gmail.com>
2025-07-03 00:30:24 +0200

    Changed hello.py to intro.py

---
+++ intro.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+name = "Arjuna"
+age = 25
+hobby = "coding"
+print("Hi, my name is",name)
+print("I am,", age , "years old.")
+print("I love,", hobby + "Reading, and workout")
